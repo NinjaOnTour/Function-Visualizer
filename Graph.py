5048f17b00744b9f861f036cbea5dcbac8e28ff7
NinjaOnTour <70662656+NinjaOnTour@users.noreply.github.com>
2022-09-05 20:25:20 +0300

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def DrawGraph(func, max): # func: Function   ,   max: Maximum x value in graph
-
-    xList = list(range(0, max))
+# func: Function
+# min, max: Minimum and Maximum x value in graph
+# step: increase between two x values
+def DrawGraph(func, min, max, step, Title, labelX, labelY, gridOn): 
+    xList = np.arange(min, max, step)
     yList = list()
 
-    for i in range(0, max):
+    for i in xList:
         yList.append(func(i))
     
+    plt.xlabel(labelX)
+    plt.ylabel(labelY)
+    plt.grid(gridOn)
+    plt.title(Title)
     plt.plot(xList, yList)
     plt.show()
